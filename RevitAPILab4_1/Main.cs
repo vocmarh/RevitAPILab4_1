ce467dcc005c0005c5e3bbe0cc82dc5ca0f31639
Сергей Храмцов <vocmarh@mail.ru>
2022-02-18 16:18:54 +0500

    Добавьте файлы проекта.

---
+++ RevitAPILab4_1/Main.cs
@@ -0,0 +1,52 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Microsoft.Win32;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPILab4_1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            string wallInfo = string.Empty;
+
+            var walls = new FilteredElementCollector(doc)
+                .OfClass(typeof(Wall))
+                .Cast<Wall>()
+                .ToList();
+
+            foreach (Wall wall in walls)
+            {
+                double wallVolume = wall.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED).AsDouble();
+                string wallVolumeM = UnitUtils.ConvertFromInternalUnits(wallVolume, DisplayUnitType.DUT_CUBIC_METERS).ToString();
+                wallInfo += $"{wall.Name};{wallVolumeM}{Environment.NewLine}";
+            }
+
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string csvPath = Path.Combine(desktopPath, "wallInfo.csv");
+
+            File.WriteAllText(csvPath, wallInfo);
+
+
+            return Result.Succeeded;
+        }
+    }
+}
